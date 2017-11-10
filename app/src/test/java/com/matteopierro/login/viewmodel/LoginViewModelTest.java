7b48a82ca44518bcf7af5618f03db2baf3219e6b
Matteo Pierro <pierromatteo@gmail.com>
2017-11-10 13:55:23 +0100

    tests are now based on the state of the view model

--- app/src/test/java/com/matteopierro/login/viewmodel/LoginViewModelTest.java
+++ app/src/test/java/com/matteopierro/login/viewmodel/LoginViewModelTest.java
@@ -18,12 +18,16 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 
-import static com.matteopierro.login.viewmodel.FieldError.REQUIRED;
 import static com.matteopierro.login.viewmodel.FieldError.INVALID_USERNAME;
 import static com.matteopierro.login.viewmodel.FieldError.NO_ERROR;
+import static com.matteopierro.login.viewmodel.FieldError.REQUIRED;
+import static com.matteopierro.login.viewmodel.FieldError.WRONG_PASSWORD;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -34,12 +38,6 @@ public class LoginViewModelTest {
 
     @Mock
     private Observer<Boolean> progress;
-    @Mock
-    private Observer<FieldError> usernameError;
-    @Mock
-    private Observer<FieldError> passwordError;
-    @Mock
-    private Observer<Boolean> loginSuccess;
 
     @Mock
     private UserRepository users;
@@ -51,40 +49,28 @@ public class LoginViewModelTest {
         when(users.findBy(anyString())).thenReturn(Observable.<User>empty());
         loginViewModel = new LoginViewModel(users);
         loginViewModel.progress().observeForever(progress);
-        loginViewModel.usernameError().observeForever(usernameError);
-        loginViewModel.passwordError().observeForever(passwordError);
-        loginViewModel.loginSuccess().observeForever(loginSuccess);
-    }
-
-    @Test
-    public void should_be_in_progress_during_login_attempt() throws Exception {
-        loginViewModel.login("username", "password");
-
-        InOrder inOrder = inOrder(progress);
-        inOrder.verify(progress).onChanged(true);
-        inOrder.verify(progress).onChanged(false);
     }
 
     @Test
     public void should_clean_all_errors() throws Exception {
         loginViewModel.login("username", "password");
 
-        verify(usernameError).onChanged(NO_ERROR);
-        verify(passwordError).onChanged(NO_ERROR);
+        assertEquals(NO_ERROR, loginViewModel.usernameError().getValue());
+        assertEquals(NO_ERROR, loginViewModel.passwordError().getValue());
     }
 
     @Test
     public void username_error_when_username_is_empty() throws Exception {
         loginViewModel.login("", "password");
 
-        verify(usernameError).onChanged(REQUIRED);
+        assertThat(loginViewModel.usernameError().getValue(), is(REQUIRED));
     }
 
     @Test
     public void password_error_when_username_is_empty() throws Exception {
         loginViewModel.login("username", "");
 
-        verify(passwordError).onChanged(REQUIRED);
+        assertThat(loginViewModel.passwordError().getValue(), is(REQUIRED));
     }
 
     @Test
@@ -94,7 +80,7 @@ public class LoginViewModelTest {
 
         loginViewModel.login("unknown username", "password");
 
-        verify(usernameError).onChanged(INVALID_USERNAME);
+        assertThat(loginViewModel.usernameError().getValue(), is(INVALID_USERNAME));
     }
 
     @Test
@@ -110,7 +96,7 @@ public class LoginViewModelTest {
 
         loginViewModel.login("username", "wrong password");
 
-        verify(passwordError).onChanged(FieldError.WRONG_PASSWORD);
+        assertThat(loginViewModel.passwordError().getValue(), is(WRONG_PASSWORD));
     }
 
     @Test
@@ -126,6 +112,15 @@ public class LoginViewModelTest {
 
         loginViewModel.login("username", "password");
 
-        verify(loginSuccess).onChanged(true);
+        assertTrue(loginViewModel.loginSuccess().getValue());
+    }
+
+    @Test
+    public void should_be_in_progress_during_login_attempt() throws Exception {
+        loginViewModel.login("username", "password");
+
+        InOrder inOrder = inOrder(progress);
+        inOrder.verify(progress).onChanged(true);
+        inOrder.verify(progress).onChanged(false);
     }
 }
