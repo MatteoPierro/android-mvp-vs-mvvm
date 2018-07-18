cb4ec5fe42568f7d804888e2f7f7db949952f199
Matteo Pierro <matteo.pierro@bnpparibasfortis.com>
2018-07-18 11:21:07 +0200

    Removed duplication in tests

--- app/src/test/java/com/matteopierro/login/presenter/LoginPresenterTest.java
+++ app/src/test/java/com/matteopierro/login/presenter/LoginPresenterTest.java
@@ -2,18 +2,29 @@ package com.matteopierro.login.presenter;
 
 import com.matteopierro.login.view.LoginView;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
+
+    @Mock
+    private LoginView view;
+    private LoginPresenter presenter;
+
+    @Before
+    public void setUp() throws Exception {
+        presenter = new LoginPresenter(view);
+    }
+
     @Test
     public void shouldDisplayEmptyUsernameErrorWhenUsernameEmpty() {
-        LoginView view = mock(LoginView.class);
-        LoginPresenter presenter = new LoginPresenter(view);
-
         presenter.login("");
 
         verify(view).displayEmptyUserNameError();
@@ -21,9 +32,6 @@ public class LoginPresenterTest {
 
     @Test
     public void shouldNotDisplayEmptyUsernameErrorWhenUsernameIsNotEmpty() {
-        LoginView view = mock(LoginView.class);
-        LoginPresenter presenter = new LoginPresenter(view);
-
         presenter.login("username");
 
         verify(view, never()).displayEmptyUserNameError();
